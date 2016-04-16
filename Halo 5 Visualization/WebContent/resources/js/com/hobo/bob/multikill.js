224a8c2cc0417b6959c14b215782e74dec7f92e9
Daniel McFarland <doctordan81@gmail.com>
2016-04-16 17:36:49 -0400

    Added styles for more multikills. Changed loadMultiKills to accept data in array format and add all to the map.

--- Halo 5 Visualization/WebContent/resources/js/com/hobo/bob/multikill.js
+++ Halo 5 Visualization/WebContent/resources/js/com/hobo/bob/multikill.js
@@ -1,43 +1,125 @@
-function loadMultiKills() {
-	addLayer(multiKill3, new ol.style.Style({
-		image : new ol.style.Circle({
-			radius : 5,
-			snapToPixel : false,
-			fill : new ol.style.Fill({
-				color : '#008080'
-			}),
-			stroke : new ol.style.Stroke({
-				color : '#2F4F4F',
-				width : 1
-			})
+var multiKillStyles = [
+new ol.style.Style({
+	image : new ol.style.Circle({
+		radius : 5,
+		snapToPixel : false,
+		fill : new ol.style.Fill({
+			color : '#000080'
+		}),
+		stroke : new ol.style.Stroke({
+			color : '#87CEEB',
+			width : 1
 		})
-	}));
-	
-	addLayer(multiKill2, new ol.style.Style({
-		image : new ol.style.Circle({
-			radius : 5,
-			snapToPixel : false,
-			fill : new ol.style.Fill({
-				color : '#000080'
-			}),
-			stroke : new ol.style.Stroke({
-				color : '#87CEEB',
-				width : 1
-			})
+	})
+}),
+new ol.style.Style({
+	image : new ol.style.Circle({
+		radius : 5,
+		snapToPixel : false,
+		fill : new ol.style.Fill({
+			color : '#008080'
+		}),
+		stroke : new ol.style.Stroke({
+			color : '#2F4F4F',
+			width : 1
 		})
-	}));
-	
-	addLayer(multiKill4, new ol.style.Style({
-		image : new ol.style.Circle({
-			radius : 5,
-			snapToPixel : false,
-			fill : new ol.style.Fill({
-				color : '#2E8B57'
-			}),
-			stroke : new ol.style.Stroke({
-				color : '#00FF7F',
-				width : 1
-			})
+	})
+}),
+new ol.style.Style({
+	image : new ol.style.Circle({
+		radius : 5,
+		snapToPixel : false,
+		fill : new ol.style.Fill({
+			color : '#2E8B57'
+		}),
+		stroke : new ol.style.Stroke({
+			color : '#00FF7F',
+			width : 1
 		})
-	}));
+	})
+}),
+new ol.style.Style({
+	image : new ol.style.Circle({
+		radius : 5,
+		snapToPixel : false,
+		fill : new ol.style.Fill({
+			color : '#32CD32'
+		}),
+		stroke : new ol.style.Stroke({
+			color : '#ADFF2F',
+			width : 1
+		})
+	})
+}),
+new ol.style.Style({
+	image : new ol.style.Circle({
+		radius : 5,
+		snapToPixel : false,
+		fill : new ol.style.Fill({
+			color : '#FFFF00'
+		}),
+		stroke : new ol.style.Stroke({
+			color : '#808000',
+			width : 1
+		})
+	})
+}),
+new ol.style.Style({
+	image : new ol.style.Circle({
+		radius : 5,
+		snapToPixel : false,
+		fill : new ol.style.Fill({
+			color : '#FFD700'
+		}),
+		stroke : new ol.style.Stroke({
+			color : '#B8860B',
+			width : 1
+		})
+	})
+}),
+new ol.style.Style({
+	image : new ol.style.Circle({
+		radius : 5,
+		snapToPixel : false,
+		fill : new ol.style.Fill({
+			color : '#D2691E'
+		}),
+		stroke : new ol.style.Stroke({
+			color : '#8B4513',
+			width : 1
+		})
+	})
+}),
+new ol.style.Style({
+	image : new ol.style.Circle({
+		radius : 5,
+		snapToPixel : false,
+		fill : new ol.style.Fill({
+			color : '#FF4500'
+		}),
+		stroke : new ol.style.Stroke({
+			color : '#A52A2A',
+			width : 1
+		})
+	})
+}),
+new ol.style.Style({
+	image : new ol.style.Circle({
+		radius : 5,
+		snapToPixel : false,
+		fill : new ol.style.Fill({
+			color : '#800000'
+		}),
+		stroke : new ol.style.Stroke({
+			color : '#A52A2A',
+			width : 1
+		})
+	})
+})
+];
+
+function loadMultiKills(data) {
+	for (var idx in data) {
+		addLayer(data[idx], multiKillStyles[idx]);
+	}
 }
