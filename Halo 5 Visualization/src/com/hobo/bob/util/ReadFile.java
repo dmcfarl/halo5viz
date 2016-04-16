3c4c90f10ce2ce6287d13e72e3a54f71cb2191f4
Daniel McFarland <doctordan81@gmail.com>
2016-04-16 15:49:32 -0400

    Added new common util for reading files.

---
+++ Halo 5 Visualization/src/com/hobo/bob/util/ReadFile.java
@@ -0,0 +1,32 @@
+package com.hobo.bob.util;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFile {
+	protected String read(String filename) {
+		StringBuffer data = new StringBuffer();
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(filename));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				data.append(line);
+				data.append("\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return data.toString();
+	}
+}
